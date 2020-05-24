16e93c119705736f3a7b21a94f044792c8a4ed5b
spmskperera <spmskperera@gmail.com>
2020-05-24 12:51:33 +0530

    Fixes env varible of namespace on srver and axios base url on client

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,5 @@
 // ADD HERE VALUE BEFORE DEPLOY
 module.exports = {
   DB_URI: process.env.DB_URI,
+  NAMESPACE: "https://susanthaperera.herokuapp.com",
 };
--- server/services/auth.js
+++ server/services/auth.js
@@ -1,7 +1,8 @@
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 
-const nameSpace = process.env.NAMESPACE; //"http://localhost:3000/";
+const config=require('../config');
+const nameSpace = config.NAMESPACE; //"http://localhost:3000/";
 //MIDDLEWARE
 exports.checkJWT = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -19,7 +20,7 @@ exports.checkJWT = jwt({
 exports.checkRole = (role) => {
   return (req, res, next) => {
     const user = req.user;
-    if (user && user[nameSpace + "/role"] === role) {
+    if (user && user[nameSpace + "/role"] && user[nameSpace + "/role"] === role) {
       next();
     } else {
       return res.status(401).send({
